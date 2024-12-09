8e6e7820602ccdd7230b706e9e545432d56b04ca
FuanFrost <fuanfrost@gmail.com>
2024-12-09 15:04:51 -0500

    bullet destruction
    bullet destroys it self depending on the the tag

--- FallJam2024/Assets/Scripts/juan/player2Movement.cs
+++ FallJam2024/Assets/Scripts/juan/player2Movement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class player1Movement : MonoBehaviour
+public class player2Movement : MonoBehaviour
 {
     public GameObject spellProjectile; // The spell object to be instantiated
 
@@ -21,7 +21,7 @@ public class player1Movement : MonoBehaviour
         HandleMovement();
 
         // Handle spell casting when Space is pressed
-        if (Keyboard.current.rightShiftKey.wasPressedThisFrame)
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             CastSpell();
         }
@@ -32,13 +32,13 @@ public class player1Movement : MonoBehaviour
     {
         // Get keyboard input
         float horizontal = 0f;
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -1f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
+        if (Keyboard.current.aKey.isPressed)
         {
             horizontal = 1f;
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            horizontal = -1f;
         }
 
         // Move the player horizontally
@@ -55,8 +55,8 @@ public class player1Movement : MonoBehaviour
     // Instantiates and casts a spell
     void CastSpell()
     {
-        Vector3 spellPosition = transform.position + new Vector3(0, 0.5f, 0); // Offset for the spell spawn position
-        GameObject spell = Instantiate(spellProjectile, spellPosition, Quaternion.identity); // Create the spell
-        spell.GetComponent<Rigidbody2D>().velocity = new Vector2(0, spellSpeed); // Apply velocity to the spell
+        Vector3 spellPosition = transform.position + new Vector3(0, -0.5f, 0); // Offset for the spell spawn position
+        GameObject spell = Instantiate(spellProjectile, spellPosition, Quaternion.Euler(0, 0, 180)); // Create the spell
+        spell.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -spellSpeed); // Apply velocity to the spell
     }
 }
